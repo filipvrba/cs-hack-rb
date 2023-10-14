5e0d322049e84a28ea2910cd32b57c1631947e2e
Filip Vrba <filipvrba@pm.me>
2023-10-14 20:07:26 +0200

    fix uu letter

--- lib/csh/translate.rb
+++ lib/csh/translate.rb
@@ -1,19 +1,19 @@
 module CSH
   module Translate
-    LETTER = {
-      "e'": '',
-      "s'": '',
-      "c'": '',
-      "t'": '',
-      "r'": '',
-      "z'": '',
+    DEC_LETTER = {
+      "e\'": 'ě',
+      "s\'": 'š',
+      "c\'": 'č',
+      "t\'": 'ť',
+      "r\'": 'ř',
+      "z\'": 'ž',
 
-      "yy": '',
-      "aa": '',
-      "ii": '',
-      "ee": '',
-      "oo": '',
-      "uu": ['', ''],
+      "yy": 'ý',
+      "aa": 'á',
+      "ii": 'í',
+      "ee": 'é',
+      "oo": 'ó',
+      "uu": ['ú', 'ů'],
     }
 
     module_function
@@ -21,10 +21,25 @@ module CSH
     def decipher(text)
       m_result = text
 
-      
+      l_mut_result = lambda do |k, v, r=''|
+        r_down = "#{r}#{k}"
+        r_up   = "#{r}#{k.upcase}"
+        m_result = m_result.gsub(/#{r_down}/, v)
+        m_result = m_result.gsub(/#{r_up}/, v.upcase)
+      end
+      la_mut_result = lambda do |k, v|
+        l_mut_result.call(k, v[0], "(^|\s)")
+        l_mut_result.call(k, v[1])
+      end
 
-      p m_result
-      exit
+      DEC_LETTER.each do |k, v|
+        if v.is_a?(Array)
+          la_mut_result.call(k, v)
+        else
+          l_mut_result.call(k, v)
+        end
+      end
+
       m_result
     end
   end#Translate
