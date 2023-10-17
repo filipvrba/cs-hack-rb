50c48e999a6aa5c8f67b3ef8cb5bd924581ebdef
Filip Vrba <filipvrba@pm.me>
2023-10-17 09:58:26 +0200

    identified os for clipboard fn

--- lib/csh/clipboard.rb
+++ lib/csh/clipboard.rb
@@ -3,7 +3,19 @@ module CSH
     module_function
 
     def set(text)
-      system("termux-clipboard-set #{text}")
+      os = RUBY_PLATFORM
+
+      if os.index('android')
+        return system("termux-clipboard-set #{text}")
+      end
+
+      print_warning()
+      return false
+    end
+
+    def print_warning()
+      Core::Event.print('warning', "The OS has not been indedified\n" +
+                        "for the clipboard function.")
     end
   end
 end
